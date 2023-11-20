e84d2b33257e3462fb03a0028ab5a514f541a588
s96681 <thanawit.chaisuphapsirikun@scb.co.th>
2023-11-20 08:12:29 +0700

    Update: kw open chrome, pythonlib.chromeoption

--- pythonlibs/common_util.py
+++ pythonlibs/common_util.py
@@ -4,20 +4,27 @@ from selenium.webdriver.chrome.service import Service
 import os
 
 
-def open_eppd_by_chrome_browser(urlTokens):
-    driverPath = find_chromedriver_path()
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    options.add_argument("--disable-popup-blocking")
-    options.add_argument("--disable-infobars")
-    # options.add_argument("--headless")
-    options.add_experimental_option("detach", True)
-    webdriver_service = Service(driverPath)
-    driver = webdriver.Chrome(service=webdriver_service, options=options)
-    driver.get(urlTokens)
+# def open_eppd_by_chrome_browser(urlTokens):
+#     driverPath = find_chromedriver_path()
+#     options = webdriver.ChromeOptions()
+#     options.add_argument("--start-maximized")
+#     options.add_argument("--disable-popup-blocking")
+#     options.add_argument("--disable-infobars")
+#     # options.add_argument("--headless")
+#     options.add_experimental_option("detach", True)
+#     webdriver_service = Service(driverPath)
+#     driver = webdriver.Chrome(service=webdriver_service, options=options)
+#     driver.get(urlTokens)
 
 def find_chromedriver_path():
     current_path = os.path.abspath(__file__)
     chromedriverpath = os.path.abspath(os.path.join(current_path, '../../drivers/chrome_macx64_11906045105/chromedriver'))
     return chromedriverpath
 
+def chrome_options():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-popup-blocking")
+    options.add_argument("--disable-infobars")
+    options.add_experimental_option("detach", True)
+    return options
--- testcases/demo_test/test_demo.robot
+++ testcases/demo_test/test_demo.robot
@@ -4,3 +4,5 @@ Resource    ../../resources/global_keyword.resource
 *** Test Cases ***
 Test go to eppd with chrome
     Go to eppd by chrome browser    TTCUSEROP1@TTC   TTC
+    Wait Until Element Is Visible    //body/div[@id='root']/div[1]/div[1]/div[3]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/button[1]    10s
+    Close Browser
